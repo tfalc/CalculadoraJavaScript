40d2bf801c469cf18520788089a8a6b559fd5484
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-03-20 17:09:39 -0300

    bug correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
-/* Captação dos valores */
-
-let valorRisco = document.getElementById("valorRisco"); /* Recebe valor do input Valor Risco */
-let comissao = document.getElementById("comissao"); /* Recebe o valor do input Comissão */
-let premioNet = document.getElementById("valorPremioNet"); /* Recebe o valor da div Prêmio Net */
-let premioLiquido = document.getElementById("valorPremioLiquido"); /* Recebe o valor da div Prêmio Líquido */
-let valorPremioCalculado = document.getElementById("valorPremioCalculado"); /* Recebe o valor da div Premio Calculado */
-let valorLiquidoMinimo = document.getElementById("valorPremioLiquidoMinimo"); /* Recebe o valor da div Prêmio Líquido Mínimo */
-let valorLmi = document.getElementById("valorLMI"); /* Recebe o valor da div LMI */
-let valorPremioLiquido = document.getElementById("premioLiquidoInput"); /* Recebe o valor do input Premio Líquido */
-let taxaNetDiv = document.getElementById("valorTaxaNet"); /* Recebe o valor do div taxa net */
+const btn = document.querySelector("#botaoBMG");
 
 /* Gera evento listener para quando botão de cálculo for clicado */
-document.getElementById("botaoBMG").addEventListener("click", calcula);
+btn.addEventListener("click", function (e) {
 
-/* inicialização das variáveis */
-let premioNetCalculado, premioCalculado, lmi, taxaNetDivCalculo = 0;
+	e.preventDefault();
 
-/* Cálculos */
-function calcula() {
-	premioNetCalculado = (valorRisco * (1 + (comissao / 100)) * taxaNet) / 100;
-	premioCalculado = premioNet / (1 - (comissao / 100));
-	let taxaNetInput = Number(premioNet / valorRisco) * 100;
-	let premioTotal = premioLiquido * 1.0738;
-	lmi = valorRisco * (1 + (comissao / 100));
-	taxaNetDivCalculo = 1, 3 * 0, 8;
+	/* Captação dos valores */
 
-}
-/* inserção dos valores no formulário */
-document.getElementById("premioNetInput").value = premioNet; /* Atualiza input Prêmio Net*/
-document.getElementById("taxaNetInput").value = taxaNetInput; /* Atualiza input Taxa Net */
-document.getElementById("premioLiquidoInput").value = premioCalculado; /* Atualiza o input Prêmio Líquido */
-document.getElementById("premioTotalInput").value = premioTotal; /* Atualiza input Prêmio total*/
+	let valorRisco = document.querySelector("#valorRisco").value; /* Recebe valor do input Valor Risco */
+	let comissao = document.querySelector("#comissaoLabel").value; /* Recebe o valor do input Comissão */
+	let premioNet = document.getElementById("valorPremioNet"); /* Recebe o valor da div Prêmio Net */
+	let premioLiquido = document.getElementById("valorPremioLiquido"); /* Recebe o valor da div Prêmio Líquido */
+	let valorPremioCalculado = document.getElementById("valorPremioCalculado"); /* Recebe o valor da div Premio Calculado */
+	let valorLiquidoMinimo = document.getElementById("valorPremioLiquidoMinimo"); /* Recebe o valor da div Prêmio Líquido Mínimo */
+	let valorLmi = document.getElementById("valorLMI"); /* Recebe o valor da div LMI */
+	let valorPremioLiquido = document.getElementById("premioLiquidoInput"); /* Recebe o valor do input Premio Líquido */
+	let taxaNetDiv = document.getElementById("valorTaxaNet"); /* Recebe o valor do div taxa net */
 
-/* inserção dos valores na tabela*/
-//valorLmi.innerHTML = lmi;
-valorLmi.innerHTML = lmi;  /* Atualiza a div LMI com o valor do cálculo */
-taxaNetDiv.innerHTML = taxaNetDivCalculo; /* Atualiza div Taxa Net*/
-premioNet.innerHTML = premioNetCalculado; /* Atualiza div Prêmio Net */
-valorPremioCalculado.innerHTML = premioCalculado; /* Atualiza div Prêmio Calculado */
-premioLiquido.innerHTML = 'R$ ' + premioLiquido; /* Atualiza div Prêmio Líquido */
+
+	let taxaNetDivCalculo = 1.3 * 0.8;
+
+	premioNetCalculado = (Math.round(((valorRisco * (comissao / 100) * taxaNetDivCalculo) / 100) * 100) / 100).toFixed(2);
+	premioCalculado = (Math.round(premioNetCalculado / (1 - (comissao / 100)) * 100) / 100).toFixed(2);
+	let taxaNetInput = ((premioNetCalculado / valorRisco) * 100).toFixed(4);
+	let premioTotal = (Math.round((premioCalculado * 1.0738) * 100) / 100).toFixed(2);
+	lmi = (Math.round(valorRisco * (comissao / 100))).toFixed(2);
+
+	/* inserção dos valores no formulário */
+	document.getElementById("premioNetInput").value = premioNetCalculado; /* Atualiza input Prêmio Net*/
+	document.getElementById("taxaNetInput").value = taxaNetInput; /* Atualiza input Taxa Net */
+	document.getElementById("premioLiquidoInput").value = premioCalculado; /* Atualiza o input Prêmio Líquido */
+	document.getElementById("premioTotalInput").value = premioTotal; /* Atualiza input Prêmio total*/
+
+	/* inserção dos valores na tabela*/
+
+	console.log(new Intl.NumberFormat().format(premioNetCalculado));
+
+	valorLmi.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(lmi);  /* Atualiza a div LMI com o valor do cálculo */
+	taxaNetDiv.innerHTML = taxaNetDivCalculo + "%"; /* Atualiza div Taxa Net*/
+	premioNet.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(premioNetCalculado); /* Atualiza div Prêmio Net */
+	valorPremioCalculado.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(premioCalculado); /* Atualiza div Prêmio Calculado */
+	premioLiquido.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(premioCalculado); /* Atualiza div Prêmio Líquido */	
+	valorLiquidoMinimo.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(400); /* Atualiza div Prêmio Líquido */	
+});
+
+function setTwoNumberDecimal(el) {
+	el.value = parseFloat(el.value).toFixed(2);
+};
